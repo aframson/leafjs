100dd25fb872dfda450b504a19562af307de810a
aframson <obiriaframson@gmail.com>
2022-09-28 16:43:05 +0000

    done2

--- soma/configs/port.js
+++
@@ -1,8 +0,0 @@
-/** PORT SETTINGS 
- * =======================================================================
- *  Contains The PORT number and environment for the server 
- * =======================================================================
- */
-const PORT  = process.env.PORT || 5050 // you can change the port here 
-const NODE_ENV = process.env.NODE_ENV || 'development'
-module.exports = {PORT,NODE_ENV}
