e4a084a7b2414fd435dda3b58e75e1c5924daed3
aframson <obiriaframson@gmail.com>
2022-09-30 18:43:24 +0000

    changed config folder6

--- api/realtime/route.js
+++ api/realtime/route.js
@@ -10,7 +10,6 @@ module.exports = function (io) {
     // listerning for connection
     io.on('connection', (socket) => {
          console.log('user connected ', socket.id)  // checking for connected users 
-
          /**
           * Routes begins here 
           */
